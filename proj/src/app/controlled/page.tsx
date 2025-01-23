810e17a39c1ab97a4545a04a907387f9846ad623
ezacd <yaroslav.us2312@gmail.com>
2025-01-23 18:02:49 +0300

    feat: controlled&uncontrolled pages

---
+++ proj/src/app/controlled/page.tsx
@@ -0,0 +1,5 @@
+import './controlled_page.css';
+
+export default function Controlled() {
+  return <div className="controlled_form_page">Controlled</div>;
+}
---
+++ proj/src/app/uncontrolled/page.tsx
@@ -0,0 +1,5 @@
+import './uncontrolled_page.css';
+
+export default function Uncontrolled() {
+  return <div className="uncontrolled_form_page">Uncontrolled</div>;
+}
