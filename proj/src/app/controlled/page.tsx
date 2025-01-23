2c374ef0ea9730cd335e2ff3808daeb8633a805e
ezacd <yaroslav.us2312@gmail.com>
2025-01-23 19:12:04 +0300

    feat: forms

--- proj/src/app/controlled/page.tsx
+++ proj/src/app/controlled/page.tsx
@@ -1,5 +1,49 @@
 import './controlled_page.css';
 
 export default function Controlled() {
-  return <div className="controlled_form_page">Controlled</div>;
+  return (
+    <div className="form_page">
+      <div className="form">
+        <div>Uncontrolled form</div>
+        <form>
+          <label>
+            Name
+            <input type="text" />
+          </label>
+          <label>
+            Age
+            <input type="text" />
+          </label>
+          <label>
+            Password
+            <input type="password" />
+          </label>
+          <label>
+            Confirm password
+            <input type="password" />
+          </label>
+          <label>
+            Gender
+            <select name="gender">
+              <option value="man">Man</option>
+              <option value="woman">Woman</option>
+            </select>
+          </label>
+          <label>
+            T&C
+            <input type="checkbox" />
+          </label>
+          <label>
+            Select image
+            <input type="file" />
+          </label>
+          <label>
+            Country
+            <input type="text" />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
