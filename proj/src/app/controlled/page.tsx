11ab4331b3ac12a8a77fd3390db0099a1b66fa08
ezacd <yaroslav.us2312@gmail.com>
2025-01-24 13:57:52 +0300

    feat: react-hook-form on the controlled form

--- proj/src/app/controlled/page.tsx
+++ proj/src/app/controlled/page.tsx
@@ -1,49 +1,77 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
 import './controlled_page.css';
 
+type FormData = {
+  name: string;
+  age: string;
+  email: string;
+  password: string;
+  confiumPassword: string;
+  gender: string;
+  tc: boolean;
+  file: FileList;
+  country: string;
+};
+
 export default function Controlled() {
+  const { register, handleSubmit } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => console.log(data);
+
   return (
     <div className="form_page">
       <div className="form">
         <div>Controlled form</div>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label>
             Name
-            <input type="text" />
+            <input type="text" {...register('name', { required: true })} />
           </label>
           <label>
             Age
-            <input type="text" />
+            <input
+              type="number"
+              {...register('age', { required: true, min: 0 })}
+            />
           </label>
           <label>
             Email
-            <input type="email" />
+            <input type="email" {...register('email', { required: true })} />
           </label>
           <label>
             Password
-            <input type="password" />
+            <input
+              type="password"
+              {...register('password', { required: true })}
+            />
           </label>
           <label>
             Confirm password
-            <input type="password" />
+            <input
+              type="password"
+              {...register('confiumPassword', { required: true })}
+            />
           </label>
           <label>
             Gender
-            <select name="gender">
+            <select {...register('gender', { required: true })}>
               <option value="man">Man</option>
               <option value="woman">Woman</option>
             </select>
           </label>
           <label className="TC">
             T&C
-            <input type="checkbox" />
+            <input type="checkbox" {...register('tc', { required: true })} />
           </label>
           <label>
             Select image
-            <input type="file" />
+            <input type="file" {...register('file', { required: true })} />
           </label>
           <label>
             Country
-            <input type="text" />
+            <input type="text" {...register('country', { required: true })} />
           </label>
           <button type="submit">Submit</button>
         </form>
